0f7d7e26131cccb4fe4195694d910daaa488ebb1
pragathithangaraju <pragathi.thangaraju@gmail.com>
2023-08-22 00:11:21 +0530

    todo nothing

--- create_backend.sh
+++ create_backend.sh
@@ -1,13 +1,16 @@
 env=$1
 aws_region=$2
-filename=$3
+folder_path=$3
+folder_path=$(cd $(dirname ${folder_path}) && pwd )
+folder_name=$(basename ${folder_path})
 
-cat <<EOF>"${i}/test.tf"
+
+cat <<EOF>"${folder_path}/backend.tf"
 data "terraform_remote_state" "pipeline1" {
   backend="s3" 
   config = {
     bucket = "testdev1"
-    key = "backend/folder1/${env}/${filename}.tfsate"
+    key = "backend/folder1/${env}/${folder_name}.tfsate"
     region = "${aws_region}"
     
   }
