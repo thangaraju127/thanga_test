9bede38552fc083c60fd638b33fda98ea6d647f5
pragathithangaraju <142438542+pragathithangaraju@users.noreply.github.com>
2023-08-22 14:29:14 +0530

    Update dev_shell.sh

--- dev_shell.sh
+++ dev_shell.sh
@@ -1,4 +1,4 @@
 environment=dev
-aws_region='us-east-1'
-access_key=AKIAYPDDECEYRHOAR4XX
-secret_key='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'
+aws_region="us-east-1"
+access_key="AKIAYPDDECEYRHOAR4XX"
+secret_key="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"
