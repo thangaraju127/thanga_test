6835849d053ba480d150770cd549f9f0f6dbd483
pragathithangaraju <pragathi.thangaraju@gmail.com>
2023-08-22 15:35:35 +0530

    test

---
+++ with_cust/variables.tf
@@ -0,0 +1,12 @@
+variable "environment" {
+    default="dev"
+}
+variable "aws_region" {
+    default="us-east-1" 
+}
+variable "access_key" {
+    default="AKIAYPDDECEYRHOAR4XX"
+}
+variable "secret_key" {
+    default="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"
+} 
